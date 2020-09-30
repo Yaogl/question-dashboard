135669e4facb1a945064b29f7b6e041812673add
姚国良 <gw00191202@corp.gwm.comm>
2020-09-30 17:23:09 +0800

    页面简单搭建

---
+++ src/layouts/globlLayout.vue
@@ -0,0 +1,54 @@
+<template>
+  <a-layout id="components-layout-demo-custom-trigger">
+    <a-layout-sider v-model:collapsed="collapsed" :trigger="null" collapsible>
+      <div class="logo">
+          题库管理
+      </div>
+      <a-menu theme="dark" mode="inline" v-model:selectedKeys="selectedKeys">
+        <a-menu-item key="1">
+          <!-- icon预留 -->
+          <span>题库管理</span>
+        </a-menu-item>
+      </a-menu>
+    </a-layout-sider>
+    <a-layout>
+      <a-layout-header style="background: #fff; padding: 0">
+        <!-- <menu-unfold-outlined
+          v-if="collapsed"
+          class="trigger"
+          @click="() => (collapsed = !collapsed)"
+        /> 收起按钮 -->
+        <!-- <menu-fold-outlined v-else class="trigger" @click="() => (collapsed = !collapsed)" /> -->
+      </a-layout-header>
+      <a-layout-content
+        :style="{ margin: '24px 16px', padding: '24px', background: '#fff', minHeight: '280px' }"
+      >
+        <slot></slot>
+      </a-layout-content>
+    </a-layout>
+  </a-layout>
+</template>
+<script>
+export default {
+  data() {
+    return {
+      selectedKeys: ['1'],
+      collapsed: false,
+    };
+  },
+};
+</script>
+<style>
+#components-layout-demo-custom-trigger{
+  height: 100%;
+}
+
+#components-layout-demo-custom-trigger .logo {
+  line-height: 40px;
+  font-size: 16px;
+  font-weight: 600;
+  background: rgba(255, 255, 255, 0.2);
+  margin: 16px;
+  color: #fff;
+}
+</style>
--- src/router/index.ts
+++ src/router/index.ts
@@ -1,19 +1,22 @@
 import { createRouter, createWebHistory, RouteRecordRaw } from 'vue-router'
 import Home from '../views/Home.vue'
+import GloblLayout from '../layouts/globlLayout.vue'
 
 const routes: Array<RouteRecordRaw> = [
   {
     path: '/',
-    name: 'Home',
-    component: Home
-  },
-  {
-    path: '/about',
-    name: 'About',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
+    name: '首页',
+    redirect: '/about',
+    component: GloblLayout,
+    children: [
+      {
+        path: '/about',
+        component: import(/* webpackChunkName: "about" */ '../views/About.vue'),
+        meta: {
+          title: '云主机'
+        }
+      }
+    ]
   }
 ]
 
